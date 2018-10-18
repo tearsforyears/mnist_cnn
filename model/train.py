bd2f88de25d523e1fd0f2044d400c16061f86cae
tearsforyears <1027664894@qq.com>
2018-10-18 22:59:55 +0800

    train a little bit

--- model/train.py
+++ model/train.py
@@ -2,6 +2,23 @@
 import tensorflow as tf
 import numpy as np
 from settings import *
-from model.inference import forward
+from .inference import forward
+from preprocessing.load import load
 
-# forward(input, train=None, regularizer=None, dropout_rate=DROPOUT_RATE)
+'''
+    forward(input, train=None, regularizer=None, dropout_rate=DROPOUT_RATE)
+'''
+
+
+def train():
+    x = tf.placeholder(name="input_x", dtype=tf.float32, shape=[None, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_DEEP])
+    y_ = tf.placeholder(name="input_y", dtype=tf.float32, shape=[None, RESULT_KIND])
+    # this is for train x and y
+    
+
+def main():
+    x, y = load()
+
+
+if __name__ == '__main__':
+    main()
